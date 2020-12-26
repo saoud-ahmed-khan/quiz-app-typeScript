2f14044bc8fe0ef9266c2e75256ec1ab8d206aa2
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-27 00:35:06 +0500

    shuffling

--- src/components/api/Api.ts
+++ src/components/api/Api.ts
@@ -1,10 +1,28 @@
-export var data:[];
-export const FetchingData= async()=>
+import { RenderQuestion } from "../RenderQuestion";
+
+export type Question=
 {
-    data = await(await fetch(`https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple`)).json();
+    category:string,
+    correct_answer:string,
+    difficulty:string,
+    incorrect_answers:string[],
+    question:string,
+}
+export type QuestinState=Question & {answer: string[]}
+export const FetchingData= async()=>
+
+{
+    let data = await(await fetch(`https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple`)).json();
     console.log(data);
-    return data
+    return data.result.map((question:Question)=>({
+        ...question,
+    })
+   )
     
+
+}
+const arrayShuffle=()=>
+{
 
 }
 
